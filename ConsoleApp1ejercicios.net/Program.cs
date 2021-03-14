b8ad788afe52d878c4da03b5b5e3a6b6c4380139
sebastian <sebasguzman147@hotmail.com>
2021-03-14 12:28:35 -0500

    mi primer repositorio

---
+++ ConsoleApp1ejercicios.net/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1ejercicios.net
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            int number = 0;
+
+            Console.WriteLine("Introduce un número:");
+            number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(number + "x" + i + "=" + (i * number));
+            }
+
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+	
+}
+
